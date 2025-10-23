c88f2c3a9fdbe58f74009c57f14db958bda15653
ayangd <jimmyhendrixx90@gmail.com>
2025-10-23 14:39:41 +0700

    Add some comments on lazygit.nix

--- programs/lazygit.nix
+++ programs/lazygit.nix
@@ -8,10 +8,14 @@
         # pager = "delta --dark --paging=never";
         # externalDiffCommand = "difft --color=always --display=inline";
       };
+
+      # Default configurations before breaking changes
       autoForwardBranches = "none";
       localBranchSortOrder = "recency";
       remoteBranchSortOrder = "alphabetical";
     };
+
+    # Default configurations before breaking changes
     gui = {
       useHunkModeInStagingView = false;
     };
