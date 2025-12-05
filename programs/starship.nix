9094a3c8ac5d365aac0516c19dcfec8f47da5d48
ayangd <jimmyhendrixx90@gmail.com>
2025-12-05 14:28:29 +0700

    feat(starship): add shell indicator

--- programs/starship.nix
+++ programs/starship.nix
@@ -7,5 +7,14 @@
       success_symbol = "[➜](bold green)";
       error_symbol = "[➜](bold red)";
     };
+    shell = {
+      disabled = false;
+      format = "[$indicator $name]($style) ";
+      style = "bold blue";
+      bash_indicator = "";
+      zsh_indicator = "⚡";
+      fish_indicator = "󰈺";
+      nu_indicator = "⟡";
+    };
   };
 }
