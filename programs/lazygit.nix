4bff104e8586d3480e9027e1df51e97721279155
ayangd <jimmyhendrixx90@gmail.com>
2026-04-07 17:21:17 +0700

    fix(lazygit): migrate lazygit configuration to match expected structure

--- programs/lazygit.nix
+++ programs/lazygit.nix
@@ -3,11 +3,13 @@
   enable = true;
   settings = {
     git = {
-      paging = {
-        colorArg = "always";
-        # pager = "delta --dark --paging=never";
-        # externalDiffCommand = "difft --color=always --display=inline";
-      };
+      pagers = [
+        {
+          colorArg = "always";
+          # pager = "delta --dark --paging=never";
+          # externalDiffCommand = "difft --color=always --display=inline";
+        }
+      ];
 
       # Default configurations before breaking changes
       autoForwardBranches = "none";
